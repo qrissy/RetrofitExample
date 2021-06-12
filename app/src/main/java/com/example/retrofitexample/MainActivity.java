d54de24b711db76b4cfe02f465df791dc604be7f
qrissy <qris09@gmail.com>
2021-06-12 23:52:33 +0100

    Post Class updated

--- app/src/main/java/com/example/retrofitexample/MainActivity.java
+++ app/src/main/java/com/example/retrofitexample/MainActivity.java
@@ -35,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
          jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
         //getPosts();
-        getComments();
+        //getComments();
+        createPost();
     }
 
     private void getPosts() {
@@ -107,6 +108,39 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void createPost() {
+        Post post = new Post(23, "New Title", "New Text");
+
+        Call<Post> call = jsonPlaceHolderApi.createPost(post);
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+
+                if(!response.isSuccessful()) {
+                    textViewResult.setText("code: " + response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+
+                String content ="";
+                content += "Code: " + response.code() + "\n";
+                content += "ID: " + postResponse.getId() + "\n";
+                content += "User ID: " + postResponse.getUserId() + "\n";
+                content += "Title: " + postResponse.getTitle() + "\n";
+                content += "Text: " + postResponse.getText() + "\n\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
--- app/src/main/java/com/example/retrofitexample/Post.java
+++ app/src/main/java/com/example/retrofitexample/Post.java
@@ -4,21 +4,27 @@ import com.google.gson.annotations.SerializedName;
 
 public class Post {
 
-    private int UserId;
+    private int userId;
 
-    private int Id;
+    private Integer id;
 
     private String title;
 
     @SerializedName("body")
     private String text;
 
+    public Post(int userId, String title, String text) {
+        this.userId = userId;
+        this.title = title;
+        this.text = text;
+    }
+
     public int getUserId() {
-        return UserId;
+        return userId;
     }
 
     public int getId() {
-        return Id;
+        return id;
     }
 
     public String getTitle() {
